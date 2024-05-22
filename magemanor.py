3def9d4e253f84d8ecdb77abe7f7c5f8eb010a83
MatteoRoscioli <165591001+MatteoRoscioli@users.noreply.github.com>
2024-05-22 21:49:03 +0000

    i give up i cant figure out ANYTHING jax

--- magemanor.py
+++ magemanor.py
@@ -16,6 +16,7 @@ while True:
     choice1 = input()
 
     if choice1 == "1":
+        choice1 = 0
         print("Health: " + str(player_health), ", Shield: " + str(player_shield))
         print("You have entered the deep, dark cave...\n Before you, you see " +
               "1) A large cave tunnel with mysterious things inside...\n" +
@@ -46,47 +47,46 @@ while True:
                 player_shield += 30
                 
             print("Health: " + str(player_health), ", Shield: " + str(player_shield))
-            break
+
 
     print("THE CREEPER IS HERE!!!! WHAT DO YOU DO?\n" +
       "1) FIGHT!")
-
+    
 
 
     while True:
-        if input():
             print("Health: " + str(player_health) + ", Shield: " + str(player_shield) + ", Creeper Health: " + str(
             creeper_health))
-        print(
+            print(
             "What do you do?\n" +
             "1) Cast lightning bolt\n" +
             "2) Cast fireball\n" +
             "3) Heal\n")
-        choice3 = input()
-        if choice3 == "3":
-            player_shield += 30
-        elif choice3 == "2":
-            player_shield -= 50
-            player_shield -= 5 * 10
-            creeper_health -= 100
-            print("You cast fireball... You lost 100 Shield! The creeper lost 100 health!")
-        elif choice3 == "1":
-            player_shield -= 25
-            creeper_health -= 50
-            print("You cast lightning bolt. The creeper kind of hurts. You lost 25 shield!")
+            choice3 = input()
+            if choice3 == "3":
+                player_shield += 30
+            elif choice3 == "2":
+                player_shield -= 50
+                player_shield -= 5 * 10
+                creeper_health -= 100
+                print("You cast fireball... You lost 100 Shield! The creeper lost 100 health!")
+            elif choice3 == "1":
+                player_shield -= 25
+                creeper_health -= 50
+                print("You cast lightning bolt. The creeper kind of hurts. You lost 25 shield!")
 
-        if creeper_health <= 0:
-            while True:
-                print("Health: " + str(player_health) + ", Shield: " + str(player_shield))
-                print("The creeper ignites and exploded! The mini creepers attack!\n" +
+            if creeper_health <= 0:
+                while True:
+                    print("Health: " + str(player_health) + ", Shield: " + str(player_shield))
+                    print("The creeper ignites and exploded! The mini creepers attack!\n" +
                       "What do you do?\n" +
                       "1) Cast lightning bolt\n" +
                       "2) Cast fireball\n" +
                       "3) Heal\n")
-                choice4 = input()
-                if choice4 == "1" or choice4 == "2":
-                    player_shield += 50
-                    print("You defeated the creeper and its mini minions! Congratulations!\n You head towards the mansion.\n" +
+                    choice4 = input()
+                    if choice4 == "1" or choice4 == "2":
+                        player_shield += 50
+                        print("You defeated the creeper and its mini minions! Congratulations!\n You head towards the mansion.\n" +
                           "You find a maze, inside the mansion. What do you do?\n" +
                           "1) Go left\n" +
                           "2) Go straight\n" +
@@ -105,31 +105,35 @@ while True:
                     print("You healed! +30 Health!")
                     break
 
-        if choice1 == "2":
-            print("Health: " + str(player_health), ", Shield: " + str(player_shield))
-        print("You have entered the forbidden desert. \n Before you, you see " +
+
+
+            if choice1 == "2":
+                choice1 = 0
+                print("Health: " + str(player_health), ", Shield: " + str(player_shield))
+                print("You have entered the forbidden desert. \n Before you, you see " +
               "1) A road, cutting straight through the desert\n" +
               "2) A perilous cliff road\n" +
               "3) Contact Village and ask to leave\n")
-        choice4 = input()
-        if choice4 == "3":
-            print("")
-        elif choice4 == "2":
-            print("You start walking on the cliff road. You fall of the cliff and die.")    
-        elif choice4 == "1":
-            print("Health: " + str(player_health), ", Shield: " + str(player_shield))
-        if choice1 == "3":
-            print("Health: " + str(player_health), ", Shield: " + str(player_shield))
-        print("You have entered the enchanted forest...\n Before you, you see " +
+            choice5 = input()
+            if choice5 == "3":
+                print("")
+            elif choice5 == "2":
+                print("You start walking on the cliff road. You fall of the cliff and die.")    
+            elif choice5 == "1":
+                print("Health: " + str(player_health), ", Shield: " + str(player_shield))
+
+            if choice1 == "3":
+                print("Health: " + str(player_health), ", Shield: " + str(player_shield))
+            print("You have entered the enchanted forest...\n Before you, you see " +
               "1) An evil graveyard, with a frog waiting to be saved from the evil surrouning it...\n" +
               "2) Stay on same path\n" +
               "3) Contact Village and ask to leave\n")
-        choice2 = input()
-        if choice2 == "3":
-            print("")
-        elif choice2 == "1":
-            print("Health: " + str(player_health), ", Shield: " + str(player_shield))                
-            print(
+            choice2 = input()
+            if choice2 == "3":
+                print("")
+            elif choice2 == "1":
+                print("Health: " + str(player_health), ", Shield: " + str(player_shield))                
+                print(
                 "You start to walk to the graveyard, but all of a sudden... Monsters and the annoying mosquitos take notice and start to attack! What do you do?\n" +
                 "1) Cast lightning bolt\n" +
                 "2) Cast fireball\n" +
@@ -146,60 +150,57 @@ while True:
                     "You cast lightning bolt. Rain starts to pour down! The zombies hurt you. You continue down the path.")
 
             print("Health: " + str(player_health), ", Shield: " + str(player_shield))
-            break
 
-    print("THE CREEPER IS HERE!!!! WHAT DO YOU DO?\n" +
+            print("THE CREEPER IS HERE!!!! WHAT DO YOU DO?\n" +
       "1) FIGHT!")
-
-
-
-    while True:
-        if input():
-            print("Health: " + str(player_health) + ", Shield: " + str(player_shield) + ", Creeper Health: " + str(
+            while True:
+                if input():
+                    print("Health: " + str(player_health) + ", Shield: " + str(player_shield) + ", Creeper Health: " + str(
             creeper_health))
-        print(
+                print(
             "What do you do?\n" +
             "1) Cast lightning bolt\n" +
             "2) Cast fireball\n" +
             "3) Heal\n")
-        choice3 = input()
-        if choice3 == "3":
-            player_shield += 30
-        elif choice3 == "2":
-            player_shield -= 50
-            player_shield -= 5 * 10
-            creeper_health -= 100
-            print("You cast fireball... You lost 100 Shield! The creeper lost 100 health!")
-        elif choice3 == "1":
-            player_shield -= 25
-            creeper_health -= 50
-            print("You cast lightning bolt. The creeper kind of hurts. You lost 25 shield!")
+                choice3 = input()
+                if choice3 == "3":
+                    player_shield += 30
+                elif choice3 == "2":
+                    player_shield -= 50
+                    player_shield -= 5 * 10
+                    creeper_health -= 100
+                    print("You cast fireball... You lost 100 Shield! The creeper lost 100 health!")
+                elif choice3 == "1":
+                    player_shield -= 25
+                    creeper_health -= 50
+                    print("You cast lightning bolt. The creeper kind of hurts. You lost 25 shield!")
 
-        if creeper_health <= 0:
-            while True:
-                print("Health: " + str(player_health) + ", Shield: " + str(player_shield))
-                print("The creeper ignites and exploded! The mini creepers attack!\n" +
+                if creeper_health <= 0:
+                    while True:
+                        print("Health: " + str(player_health) + ", Shield: " + str(player_shield))
+                        print("The creeper ignites and exploded! The mini creepers attack!\n" +
                       "What do you do?\n" +
                       "1) Cast lightning bolt\n" +
                       "2) Cast fireball\n" +
                       "3) Heal\n")
-                choice4 = input()
-                if choice4 == "1" or choice4 == "2":
-                    player_shield += 50
-                    print("You defeated the creeper and its mini minions! Congratulations!\n You head towards the mansion.\n" +
+                        choice4 = input()
+                        if choice4 == "1" or choice4 == "2":
+                            player_shield += 50
+                            print("You defeated the creeper and its mini minions! Congratulations!\n You head towards the mansion.\n" +
                           "You find a maze, inside the mansion. What do you do?\n" +
                           "1) Go left\n" +
                           "2) Go straight\n" +
                           "3) Go right")
-                    if input() == "1":
-                        print("Theres an AMBUSH! You die. Sorry! You have a nice funeral at the village of Doom. You should have listened to Mom.")
-                        break
-                    elif input() == "3":
-                        print("You die to a pitfall trap filled with zombies. Your body is never found. You should have listened to Mom.")
-                        break
-                    elif input() == "2":
-                        print("You found the treasure! Thank God you listened to your mom! The end........")
-                        break
+                        if input() == "1":
+                            print("Theres an AMBUSH! You die. Sorry! You have a nice funeral at the village of Doom. You should have listened to Mom.")
+                            break
+                        elif input() == "3":
+                            print("You die to a pitfall trap filled with zombies. Your body is never found. You should have listened to Mom.")
+                            break
+                        elif input() == "2":
+                            print("You found the treasure! Thank God you listened to your mom! The end........")
+                            break
+                        
                 if choice4 == "3":
                     player_health += 30
                     print("You healed! +30 Health!")
